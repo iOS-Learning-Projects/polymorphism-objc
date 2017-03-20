326fb6ac5afddd02b3b49465165b8d3fb02784b1
Eduardo Vital Alencar Cunha <vital@Mac308.local>
2017-03-20 14:40:06 -0300

    Initial Commit

---
+++ Project Polimorifsmo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project Polimorifsmo
+//
+//  Created by Eduardo Vital Alencar Cunha on 20/03/17.
+//  Copyright © 2017 Vital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
